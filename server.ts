eae856f1bb843831e1fb57668726336c99c2dfee
arsalan.ahmed <arsalanabc@gmail.com>
2022-03-19 23:54:13 -0400

    Code Refactor

--- server.ts
+++ server.ts
@@ -1,6 +1,12 @@
 import * as express from 'express';
-import ApiWrapper from './src/ApiWrapper';
 require('dotenv').config();
+import ApiWrapper from './src/ApiWrapper';
+import {
+  gamesFilterByTeamId,
+  gamesFilterOutByTeamId,
+  sortDoubleHeaderGames,
+  sortNonDoubleHeaderGames,
+} from './src/GamesSorter';
 
 const app = express();
 const port = 3000;
@@ -14,84 +20,30 @@ app.get('/sort-games', async (req, res) => {
   try {
     const { teamId, date } = req.query;
 
-    const games = await apiWrapper.getGames(date as string);
-    const filteredGames = gamesFilterByTeamId(teamId as string, games);
-    const doubleHeaderFirst = sortByDoubleHeaderGames(filteredGames);
+    const gamesFromMLBStats = await apiWrapper.getGames(date as string);
+    const games = gamesFromMLBStats.dates[0].games;
+    const filteredGames = gamesFilterByTeamId(
+      parseInt(teamId as string),
+      games,
+    );
+    const doubleHeaderGames = sortDoubleHeaderGames(filteredGames);
+    const nonDoubleHeaderGames = sortNonDoubleHeaderGames(filteredGames);
 
-    const filteredOutGames = gamesFilterOutByTeamId(teamId as string, games);
-    games.dates[0].games = [...doubleHeaderFirst, ...filteredOutGames];
+    const filteredOutGames = gamesFilterOutByTeamId(
+      parseInt(teamId as string),
+      games,
+    );
+
+    gamesFromMLBStats.dates[0].games = [
+      ...doubleHeaderGames,
+      ...nonDoubleHeaderGames,
+      ...filteredOutGames,
+    ];
     res.header('Content-Type', 'application/json');
-    res.send(JSON.stringify(games, null, 2));
+    res.send(JSON.stringify(gamesFromMLBStats, null, 2));
   } catch (error) {}
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-function gamesFilterByTeamId(teamId: string, games: any) {
-  return games.dates[0].games.filter((g: any) => {
-    return g.teams.away.team.id == teamId || g.teams.home.team.id == teamId;
-  });
-}
-function gamesFilterOutByTeamId(teamId: string, games: any) {
-  return games.dates[0].games.filter((g: any) => {
-    return g.teams.away.team.id != teamId && g.teams.home.team.id != teamId;
-  });
-}
-
-function sortByDoubleHeaderGames(games: any) {
-  const singleSessionDhGames = games.filter((g: any) => {
-    return g.doubleHeader == 'Y';
-  });
-
-  const splitSessionDhGames = games.filter((g: any) => {
-    return g.doubleHeader == 'S';
-  });
-
-  const nonDoubleHeaderGames = games.filter((g: any) => {
-    return g.doubleHeader == 'N';
-  });
-  return [
-    ...sortSingleSessionDhGames(singleSessionDhGames),
-    ...sortSplitSessionDhGames(splitSessionDhGames),
-    ...nonDoubleHeaderGames,
-  ];
-}
-
-function sortSingleSessionDhGames(games: any[]) {
-  if (games.length == 0) return [];
-
-  const firstGame = games.filter((g: any) => {
-    return g.status.startTImeTBD == 'false';
-  });
-
-  const secondGame = games.filter((g: any) => {
-    return g.status.startTImeTBD == 'true';
-  });
-
-  return sortByLiveGame(firstGame, secondGame);
-}
-
-function sortSplitSessionDhGames(games: any) {
-  if (games.length == 0) return [];
-
-  const sortedByGameDate = games.sort((a: any, b: any) => {
-    return a.gameDate > b.gameDate ? 1 : -1;
-  });
-
-  return sortByLiveGame(sortedByGameDate[0], sortedByGameDate[1]);
-}
-
-function sortByLiveGame(gameA: any, gameB: any) {
-  return isLive(gameB) ? [gameB, gameA] : [gameA, gameB];
-}
-
-function isLive(game: any) {
-  const GAME_LENGTH_IN_SEC = 3 * 60 * 1000;
-  const currentTime = Date.now();
-  const gameTime = new Date(game.gameDate).getTime();
-  const timeDiff = gameTime - currentTime;
-
-  return timeDiff < 0 && timeDiff > -GAME_LENGTH_IN_SEC;
-}
