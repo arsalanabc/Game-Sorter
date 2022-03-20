892bfc541afec6fa334c4b216f7a073b2a95a85d
arsalan.ahmed <arsalanabc@gmail.com>
2022-03-19 21:27:37 -0400

    unit tests for sortByLive and isLive

--- __tests__/gamesorter.test.ts
+++ __tests__/gamesorter.test.ts
@@ -1,9 +1,14 @@
 import {
   gamesFilterByTeamId,
   gamesFilterOutByTeamId,
+  isLive,
+  sortByLiveGame,
 } from '../src/GamesSorter';
 
 describe('Games Sorter', () => {
+  const liveGame = { gameDate: Date.now() - 60 * 1000 * 60 }; // game started 1 minute ago
+  const pastGame = { gameDate: Date.now() - 60 * 1000 * 60 * 5 };
+  const futureGame = { gameDate: Date.now() + 60 * 1000 * 60 * 1 };
   const testGames = [
     {
       teams: {
@@ -105,6 +110,39 @@ describe('Games Sorter', () => {
       expect(g.teams.away.team.id != 111 && g.teams.home.team.id != 111).toBe(
         true,
       );
+    });
+  });
+
+  describe('isLive', () => {
+    it('should return true if game is live', () => {
+      expect(isLive({ gameDate: Date.now() - 60 * 1000 })).toBe(true); // game started 1 minute ago
+      expect(isLive({ gameDate: Date.now() - 60 * 1000 * 60 })).toBe(true); // game started 1 hour ago
+      expect(isLive({ gameDate: Date.now() - 60 * 1000 * 60 * 2.9 })).toBe(
+        true,
+      ); // game started 2.9 hours ago
+      expect(isLive({ gameDate: Date.now() - 60 * 1000 * 60 * 3 })).toBe(false); // game started 2.9 hours ago
+      expect(isLive({ gameDate: Date.now() - 60 * 1000 * 60 * 10 })).toBe(
+        false,
+      ); // game started 10 hours ago
+      expect(isLive({ gameDate: Date.now() + 1000 })).toBe(false); // game will start in 1 second
+      expect(isLive({ gameDate: Date.now() + 60 * 1000 * 60 })).toBe(false); // game will start in 1 hour
+    });
+  });
+
+  describe('sortByLiveGame', () => {
+    it('should return live game first', () => {
+      expect(sortByLiveGame(pastGame, liveGame)).toStrictEqual([
+        liveGame,
+        pastGame,
+      ]);
+      expect(sortByLiveGame(liveGame, pastGame)).toStrictEqual([
+        liveGame,
+        pastGame,
+      ]);
+      expect(sortByLiveGame(futureGame, liveGame)).toStrictEqual([
+        liveGame,
+        futureGame,
+      ]);
     });
   });
 });
--- src/GamesSorter.ts
+++ src/GamesSorter.ts
@@ -9,3 +9,16 @@ export function gamesFilterOutByTeamId(teamId: number, games: any) {
     return g.teams.away.team.id != teamId && g.teams.home.team.id != teamId;
   });
 }
+
+export function isLive(game: any) {
+  const GAME_LENGTH_IN_SEC = 3 * 60 * 60 * 1000;
+  const currentTime = Date.now();
+  const gameTime = new Date(game.gameDate).getTime();
+  const timeDiff = gameTime - currentTime;
+
+  return timeDiff < 0 && timeDiff > -GAME_LENGTH_IN_SEC;
+}
+
+export function sortByLiveGame(gameA: any, gameB: any) {
+  return isLive(gameB) ? [gameB, gameA] : [gameA, gameB];
+}
